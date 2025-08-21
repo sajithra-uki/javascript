21ef8b136aa38a529c9c3d98cfface498699e86e
sajithra-uki <sajithra.uki.stu@dreamspace.academy>
2025-08-21 09:39:29 +0530

    practice

---
+++ calculator.js
@@ -0,0 +1,20 @@
+var num1=20
+var num2=30
+
+function add(a,b){
+    console.log(a+b);
+}
+add(num1,num2)
+function subtract(a,b){
+    console.log(a-b);
+}
+subtract(num1,num2)
+function divide(a,b){
+    console.log(a/b);
+}
+divide(num1,num2)
+function mulitiplication(a,b){
+    console.log(a*b);
+}
+mulitiplication(num1,num2)
+
