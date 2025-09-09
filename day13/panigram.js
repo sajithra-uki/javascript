6a9e0ac019938c5902b5d2492a61c8277438b4c8
sajithra-uki <sajithra.uki.stu@dreamspace.academy>
2025-09-09 10:58:48 +0530

    day14

---
+++ day13/panigram.js
@@ -0,0 +1,14 @@
+let word="good man"
+function checktheword(word){
+    let Boolean=true;
+    for (let i=0; i=word.length; i--){
+        if(word.charAt(i) !==word.charAt(word.length-1-i)){
+            Boolean= false;
+           }
+    }
+        return Boolean;
+    
+         
+    }
+   console.log(checktheword(word));
+
